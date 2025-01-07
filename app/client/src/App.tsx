99fb2a18f9b0ed1282610218fb985bfc91fc4a58
nischit <22u1391@students.git.edu>
2025-01-07 23:31:03 +0530

    Initialized react-router-dom

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,12 +1,30 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/(auth)/Signin'
+import Landing from './pages/Landing'
+import Verify from './pages/(auth)/Verify'
 
 function App() {
 
   return (
     <>
-     <div className='text-lg'>
-      INIT
-     </div>
+     <Router>
+      <Routes>
+
+        // landing
+        <Route path='/' element={<Landing/>}/>
+
+        // home
+        <Route path="/home" element={<Home/>} />
+
+        // auth
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/signup" element={<Signin/>} />
+        <Route path="/verify" element={<Verify/>} />
+
+      </Routes>
+     </Router>
     </>
   )
 }
