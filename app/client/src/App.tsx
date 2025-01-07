c9c8d091292a21751fa33a978e2af7dedf343218
Aaryan Narayani <145103103+AaryanNarayani@users.noreply.github.com>
2025-01-07 20:12:37 +0530

    Revert "Initalized router-dom"

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,29 +1,12 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import SignUp from './pages/(auth)/SignUp'
-import Login from './pages/(auth)/Login'
-import Landing from './pages/landing'
 
 function App() {
 
   return (
     <>
-     <Router>
-        <Routes>
-
-          //landing
-          <Route path="/" element={<Landing/>} />
-
-          //home
-          <Route path="/home" element={<Home/>} />
-
-          //auth
-          <Route path="/signup" element={<SignUp/>} />
-          <Route path="/login" element={<Login/>} />
-          
-        </Routes>
-     </Router>
+     <div className='text-lg'>
+      INIT
+     </div>
     </>
   )
 }
