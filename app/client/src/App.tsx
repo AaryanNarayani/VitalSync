382623d4fa39090dc8f6e3bb222f0b5a5539e3a8
nischit <22u1391@students.git.edu>
2025-01-07 20:01:37 +0530

    Initalized router-rom

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,12 +1,29 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/(auth)/SignUp'
+import Login from './pages/(auth)/Login'
+import Landing from './pages/landing'
 
 function App() {
 
   return (
     <>
-     <div className='text-lg'>
-      INIT
-     </div>
+     <Router>
+        <Routes>
+
+          //landing
+          <Route path="/" element={<Landing/>} />
+
+          //home
+          <Route path="/home" element={<Home/>} />
+
+          //auth
+          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/login" element={<Login/>} />
+          
+        </Routes>
+     </Router>
     </>
   )
 }
