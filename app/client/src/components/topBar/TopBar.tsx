03dd1fb06df1b64c1b0306ea95847b80929031d4
Floki-07 <ziyadcbz007.com>
2025-01-13 14:15:11 +0530

    Fixed UI and some minor changes

--- app/client/src/components/topBar/TopBar.tsx
+++ app/client/src/components/topBar/TopBar.tsx
@@ -1,6 +1,6 @@
 function TopBar() {
   return (
-    <div className='flex justify-between px-4 py-2'>
+    <div className='flex justify-between px-4 py-2  w-full ' >
         <h1>VitalSync</h1>
         <button className='bg-[#8CC2F2] rounded px-3 py-1 hover:brightness-90'>Log In</button>
     </div>
